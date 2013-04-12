87be0f0a9ae685007ba5e5d04a90c6769e1fc631
Harrison Kelly <hkelly@capstrat.com>
2013-04-11 22:15:52 -0400

    Version 0.9 release!

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import find_packages
 packages = find_packages()
 
 setup(name='django-flexible-content',
-      version='0.1.0',
+      version='0.9.0',
       packages=packages,
       include_package_data=True,
       install_requires=['Django >= 1.4',
